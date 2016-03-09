2c3d322516307f1112de15c2c40e41e9bdb93e07
monkecheese <bryan.borgeson@gmail.com>
2016-03-08 16:03:23 -0800

    create previous, next, and study plan buttons that replace normal previous and next buttons on pages when '?lti_nav' is in the url. still needs to be wired up to some nonexistent javascript that will allow for navigation with these buttons within an LMS

--- themes/bombadil/functions.php
+++ themes/bombadil/functions.php
@@ -15,6 +15,7 @@ function bombadil_theme_scripts() {
   wp_enqueue_script('foundation', get_stylesheet_directory_uri() . '/js/foundation.min.js', array('jquery'), '', true);
   wp_enqueue_script('iframe_resizer', get_stylesheet_directory_uri() . '/js/iframe_resizer.js', array('jquery'), '', true);
   wp_enqueue_script('embedded_audio', get_stylesheet_directory_uri() . '/js/audio_behavior.js', array('jquery'), '', true);
+  wp_enqueue_script('lti_buttons', get_stylesheet_directory_uri() . '/js/lti_buttons.js', array('jquery'), '', true);
 }
 add_action( 'wp_enqueue_scripts', 'bombadil_theme_scripts' );
 
@@ -114,6 +115,21 @@ function ca_get_links($echo=true) {
 }
 
 /**
+ * Render LTI Previous and next buttons, for LMS Integration
+ *
+ * @param bool $echo
+ */
+function lti_get_links($echo=true){
+  if ($echo): ?>
+    <div class="lti-bottom-nav-buttons">
+        <a class="lti-nav-btn" id="lti-prev"><span class="lti-btn-arrow">&#10094;</span><span class="lti-btn-text">Previous</span></a>
+        <a class="lti-nav-btn" id="lti-next"><span class="lti-btn-text">Next</span><span class="lti-btn-arrow">&#10095;</span></a>
+        <a class="lti-nav-btn" id="study-plan">Study Plan</a>
+    </div>
+  <?php endif;
+}
+
+/**
  * Sends a Window.postMessage to resize the iframe
  * (Only works in Canvas for now)
  */
@@ -187,6 +203,14 @@ function show_nav_options($selected_option){
   }
 }
 
+function show_lti_buttons(){
+  return isset($_GET['lti_nav']);
+}
+
+function show_navigation_buttons(){
+    return show_nav_options( 'navigation_show_navigation_buttons' );
+}
+
 function show_header(){
     return show_nav_options('navigation_show_header');
 }
@@ -207,9 +231,6 @@ function show_edit_button(){
     return show_nav_options('navigation_show_edit_button');
 }
 
-function show_navigation_buttons(){
-    return show_nav_options('navigation_show_navigation_buttons');
-}
 
 function choose_logo($chosen_logo){
     $navigation = get_option( 'pressbooks_theme_options_navigation' );
