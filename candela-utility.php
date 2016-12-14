8f38285c76bda5c129ebc913228a21b34cfff97e
monkecheese <bryan.borgeson@gmail.com>
2016-12-14 11:06:39 -0800

    add revised and last modified to chapter bulk edit page

--- candela-utility.php
+++ candela-utility.php
@@ -679,3 +679,73 @@ function get_pb_page_id( $what = 'next' ) {
 
   return $post_id;
 }
+
+/*
+ * Add Revised and Last Modified to Chapter bulk edit page
+ */
+
+add_filter( 'manage_edit-chapter_columns', '\Candela\Utility\add_revised_column' );
+add_filter( 'manage_edit-chapter_sortable_columns', '\Candela\Utility\chapter_sortable_columns' );
+add_action( 'manage_chapter_posts_custom_column', '\Candela\Utility\echo_revised_column', 10, 2 );
+add_action( 'load-edit.php', '\Candela\Utility\edit_chapter_load' );
+
+function add_revised_column( $columns ) {
+  $columns['revised'] = 'Revised';
+  $columns[ 'modified_author' ] = 'Last modified by';
+
+  return $columns;
+}
+
+
+function echo_revised_column( $column, $id ) {
+  if ( 'revised' == $column )
+    echo get_post_field( 'post_modified', $id );
+
+  if ( 'modified_author' == $column ) {
+    $last_id = get_post_meta( $id, '_edit_last', TRUE );
+
+    if ( ! $last_id ) {
+      print '<i>Unknown</i>';
+      return;
+    }
+
+    $last_user = get_userdata( $last_id );
+
+    print esc_html( $last_user->display_name );
+  }
+}
+
+function chapter_sortable_columns( $columns ) {
+
+  $columns['revised'] = 'Revised';
+
+  return $columns;
+}
+
+
+function edit_chapter_load() {
+  add_filter( 'request', '\Candela\Utility\sort_chapters' );
+}
+
+/* Sorts the chapters. */
+function sort_chapters( $vars ) {
+
+  /* Check if we're viewing the 'chapter' post type. */
+  if ( isset( $vars['post_type'] ) && 'chapter' == $vars['post_type'] ) {
+
+    /* Check if 'orderby' is set to 'duration'. */
+    if ( isset( $vars['orderby'] ) && 'post_modified' == $vars['orderby'] ) {
+
+      /* Merge the query vars with our custom variables. */
+      $vars = array_merge(
+        $vars,
+        array(
+          'meta_key' => 'post_modified',
+          'orderby' => 'meta_value_num',
+        )
+      );
+    }
+  }
+
+  return $vars;
+}
