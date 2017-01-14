9fe140644f72547b9970c656ba4b12649bd11496
monkecheese <bryan.borgeson@gmail.com>
2017-01-13 19:38:41 -0800

    fix phpcs complaints for candela theme files

--- themes/candela/functions.php
+++ themes/candela/functions.php
@@ -10,9 +10,28 @@ function fitzgerald_enqueue_styles() {
 add_action( 'wp_print_styles', 'fitzgerald_enqueue_styles' );
 
 function fitzgerald_theme_scripts() {
-  wp_enqueue_script('embedded_audio', get_stylesheet_directory_uri() . '/js/audio_behavior.js', array('jquery'), '', true);
+	wp_enqueue_script( 'embedded_audio', get_stylesheet_directory_uri() . '/js/audio_behavior.js', array( 'jquery' ), '', true );
 }
 add_action( 'wp_enqueue_scripts', 'fitzgerald_theme_scripts' );
+
+function fitzgerald_enqueue_lt_ie9() {
+	global $is_IE;
+
+	if ( ! $is_IE ) {
+		return;
+	}
+
+	if ( ! function_exists( 'wp_check_browser_version' ) ) {
+		include_once( ABSPATH . 'wp-admin/includes/dashboard.php' );
+	}
+
+	$response = wp_check_browser_version();
+
+	if ( 0 > version_compare( intval( $response['version'] ), 9 ) ) {
+		wp_enqueue_script( 'fitzgerald-html5shiv', 'http://html5shim.googlecode.com/svn/trunk/html5.js', array(), 'pre3.6', false );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'fitzgerald_enqueue_lt_ie9' );
 
 /**
  * Returns an html blog of meta elements
@@ -22,10 +41,10 @@ add_action( 'wp_enqueue_scripts', 'fitzgerald_theme_scripts' );
 function pbt_get_seo_meta_elements() {
 	// map items that are already captured
 	$meta_mapping = array(
-	    'author' => 'pb_author',
-	    'description' => 'pb_about_50',
-	    'keywords' => 'pb_keywords_tags',
-	    'publisher' => 'pb_publisher'
+		'author' => 'pb_author',
+		'description' => 'pb_about_50',
+		'keywords' => 'pb_keywords_tags',
+		'publisher' => 'pb_publisher',
 	);
 
 	$html = "<meta name='application-name' content='PressBooks'>\n";
@@ -34,7 +53,7 @@ function pbt_get_seo_meta_elements() {
 	// create meta elements
 	foreach ( $meta_mapping as $name => $content ) {
 		if ( array_key_exists( $content, $metadata ) ) {
-			$html .= "<meta name='" . $name . "' content='" . $metadata[$content] . "'>\n";
+			$html .= "<meta name='" . $name . "' content='" . $metadata[ $content ] . "'>\n";
 		}
 	}
 
@@ -45,19 +64,19 @@ function pbt_get_microdata_meta_elements() {
 	// map items that are already captured
 	$html = '';
 	$micro_mapping = array(
-	    'about' => 'pb_bisac_subject',
-	    'alternativeHeadline' => 'pb_subtitle',
-	    'author' => 'pb_author',
-	    'contributor' => 'pb_contributing_authors',
-	    'copyrightHolder' => 'pb_copyright_holder',
-	    'copyrightYear' => 'pb_copyright_year',
-	    'datePublished' => 'pb_publication_date',
-	    'description' => 'pb_about_50',
-	    'editor' => 'pb_editor',
-	    'image' => 'pb_cover_image',
-	    'inLanguage' => 'pb_language',
-	    'keywords' => 'pb_keywords_tags',
-	    'publisher' => 'pb_publisher',
+		'about' => 'pb_bisac_subject',
+		'alternativeHeadline' => 'pb_subtitle',
+		'author' => 'pb_author',
+		'contributor' => 'pb_contributing_authors',
+		'copyrightHolder' => 'pb_copyright_holder',
+		'copyrightYear' => 'pb_copyright_year',
+		'datePublished' => 'pb_publication_date',
+		'description' => 'pb_about_50',
+		'editor' => 'pb_editor',
+		'image' => 'pb_cover_image',
+		'inLanguage' => 'pb_language',
+		'keywords' => 'pb_keywords_tags',
+		'publisher' => 'pb_publisher',
 	);
 	$metadata = \PressBooks\Book::getBookInformation();
 
@@ -65,9 +84,9 @@ function pbt_get_microdata_meta_elements() {
 	foreach ( $micro_mapping as $itemprop => $content ) {
 		if ( array_key_exists( $content, $metadata ) ) {
 			if ( 'pb_publication_date' == $content ) {
-				$content = date( 'Y-m-d', $metadata[$content] );
+				$content = date( 'Y-m-d', $metadata[ $content ] );
 			} else {
-				$content = $metadata[$content];
+				$content = $metadata[ $content ];
 			}
 			$html .= "<meta itemprop='" . $itemprop . "' content='" . $content . "' id='" . $itemprop . "'>\n";
 		}
@@ -81,46 +100,45 @@ function pbt_get_microdata_meta_elements() {
  * (Only works in Canvas for now)
  */
 function add_iframe_resize_message() {
-
-  printf(
-      '<script>
-    // get rid of double iframe scrollbars
-    var default_height = Math.max(
-        document.body.scrollHeight, document.body.offsetHeight,
-        document.documentElement.clientHeight, document.documentElement.scrollHeight,
-        document.documentElement.offsetHeight);
-    parent.postMessage(JSON.stringify({
-        subject: "lti.frameResize",
-        height: default_height
-    }), "*");
-</script>'
-  );
-
+	printf(
+		'<script>
+			// get rid of double iframe scrollbars
+			var default_height = Math.max(
+				document.body.scrollHeight, document.body.offsetHeight,
+				document.documentElement.clientHeight, document.documentElement.scrollHeight,
+				document.documentElement.offsetHeight);
+				parent.postMessage(JSON.stringify({
+				subject: "lti.frameResize",
+				height: default_height
+			}), "*");
+		</script>'
+	);
 }
 
 // allow iframe tag within posts
-function allow_post_tags( $allowedposttags ){
-    $allowedposttags['iframe'] = array(
-        'align' => true,
-        'allowFullScreen' => true,
-        'class' => true,
-        'frameborder' => true,
-        'height' => true,
-        'id' => true,
-        'longdesc' => true,
-        'marginheight' => true,
-        'marginwidth' => true,
-        'mozallowfullscreen' => true,
-        'name' => true,
-        'sandbox' => true,
-        'seamless' => true,
-        'scrolling' => true,
-        'src' => true,
-        'srcdoc' => true,
-        'style' => true,
-        'width' => true,
-        'webkitAllowFullScreen' => true
-    );
-    return $allowedposttags;
+function allow_post_tags( $allowedposttags ) {
+	$allowedposttags['iframe'] = array(
+		'align' => true,
+		'allowFullScreen' => true,
+		'class' => true,
+		'frameborder' => true,
+		'height' => true,
+		'id' => true,
+		'longdesc' => true,
+		'marginheight' => true,
+		'marginwidth' => true,
+		'mozallowfullscreen' => true,
+		'name' => true,
+		'sandbox' => true,
+		'seamless' => true,
+		'scrolling' => true,
+		'src' => true,
+		'srcdoc' => true,
+		'style' => true,
+		'width' => true,
+		'webkitAllowFullScreen' => true,
+	);
+
+	return $allowedposttags;
 }
-add_filter('wp_kses_allowed_html','allow_post_tags', 1);
+add_filter( 'wp_kses_allowed_html', 'allow_post_tags', 1 );
--- themes/candela/single.php
+++ themes/candela/single.php
@@ -1,66 +1,79 @@
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-<?php get_header(); ?>
-<?php if (get_option('blog_public') == '1' || (get_option('blog_public') == '0' && current_user_can_for_blog($blog_id, 'read'))): ?>
+<?php
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post();
+		get_header();
 
-				<?php edit_post_link( __( 'Edit', 'pressbooks' ), '<span class="edit-link">', '</span>' ); ?>
+		if ( get_option( 'blog_public' ) == '1' || ( get_option( 'blog_public' ) == '0' && current_user_can_for_blog( $blog_id, 'read' ) ) ) {
+			edit_post_link( __( 'Edit', 'pressbooks' ), '<span class="edit-link">', '</span>' );
+		}
+?>
 
-			<main id="main-content">
+		<main id="main-content">
+		<h1 class="entry-title">
+			<?php
+			if ( $chapter_number = pb_get_chapter_number( $post->post_name ) ) {
+				echo "<span>$chapter_number</span>  ";
+			}
+			the_title();
+			?>
+		</h1>
 
-			<h1 class="entry-title"><?php
-				if ( $chapter_number = pb_get_chapter_number( $post->post_name ) ) echo "<span>$chapter_number</span>  ";
-				the_title();
-				?></h1>
-					<?php pb_get_links(); ?>
-				<div id="post-<?php the_ID(); ?>" <?php post_class( pb_get_section_type( $post ) ); ?>>
+		<?php pb_get_links(); ?>
 
-					<div class="entry-content">
-					  <?php if ($subtitle = get_post_meta($post->ID, 'pb_subtitle', true)): ?>
-					    <h2 class="chapter_subtitle"><?php echo $subtitle; ?></h2>
-				    <?php endif;?>
-				    <?php if ($chap_author = get_post_meta($post->ID, 'pb_section_author', true)): ?>
-				       <h2 class="chapter_author"><?php echo $chap_author; ?></h2>
-			      <?php endif; ?>
+		<div id="post-<?php the_ID(); ?>" <?php post_class( pb_get_section_type( $post ) ); ?>>
+			<div class="entry-content">
+				<?php if ( $subtitle = get_post_meta( $post->ID, 'pb_subtitle', true ) ) { ?>
+					<h2 class="chapter_subtitle"><?php echo $subtitle; ?></h2>
+				<?php } ?>
+				<?php if ( $chap_author = get_post_meta( $post->ID, 'pb_section_author', true ) ) { ?>
+					<h2 class="chapter_author"><?php echo $chap_author; ?></h2>
+				<?php } ?>
 
+				<?php
+				the_content();
+				if ( get_post_type( $post->ID ) === 'part' ) {
+					echo get_post_meta( $post->ID, 'pb_part_content', true );
+				}
+				?>
+			</div><!-- .entry-content -->
+		</div><!-- #post-## -->
 
-						<?php
-						the_content();
-						if ( get_post_type( $post->ID ) === 'part' ) {
-							echo get_post_meta( $post->ID, 'pb_part_content', true );
-						} ?>
-					</div><!-- .entry-content -->
-				</div><!-- #post-## -->
-				<!-- place hidden GUIDs on applicable pages -->
-				<?php do_action('display_outcome_html', $post->ID); ?>
+		<!-- place hidden GUIDs on applicable pages -->
+		<?php do_action( 'display_outcome_html', $post->ID ); ?>
 
-				<?php if ( $citation = CandelaCitation::renderCitation( $post->ID ) ): ?>
-					<section role="contentinfo">
-						<div class="post-citations sidebar">
-							<div role="button" aria-pressed="false" id="citation-header-<?php print $post->ID; ?>" class="collapsed h3-styling"><?php _e('Licenses and Attributions'); ?></div>
-							<div id="citation-list-<?php print $post->ID; ?>" style="display:none;">
-								<?php print $citation ?>
-							</div>
-							<script>
-								jQuery( document ).ready( function( $ ) {
-									var pressed = false;
-									$( "#citation-header-<?php print $post->ID;?>" ).click(function() {
-										pressed = !pressed;
-										$( "#citation-list-<?php print $post->ID;?>" ).slideToggle();
-										$( "#citation-header-<?php print $post->ID;?>" ).toggleClass('expanded collapsed');
-										$( "#citation-header-<?php print $post->ID;?>" ).attr('aria-pressed', pressed);
-									});
-								});
-							</script>
-						</div>
-					</section>
-				<?php endif; ?>
+		<?php if ( $citation = CandelaCitation::renderCitation( $post->ID ) ) { ?>
+			<section role="contentinfo">
+				<div class="post-citations sidebar">
+					<div role="button" aria-pressed="false" id="citation-header-<?php print $post->ID; ?>" class="collapsed h3-styling"><?php _e( 'Licenses and Attributions' ); ?></div>
+					<div id="citation-list-<?php print $post->ID; ?>" style="display: none;">
+						<?php print $citation ?>
+					</div>
+					<script>
+						jQuery(document).ready(function($) {
+							var pressed = false;
+							$("#citation-header-<?php print $post->ID;?>").click(function() {
+								pressed = !pressed;
+								$("#citation-list-<?php print $post->ID;?>").slideToggle();
+								$("#citation-header-<?php print $post->ID;?>").toggleClass('expanded collapsed');
+								$("#citation-header-<?php print $post->ID;?>").attr('aria-pressed', pressed);
+							});
+						});
+					</script>
+				</div>
+			</section>
+		<?php } ?>
 
-				</div><!-- #content -->
+		</div><!-- #content -->
 
-				<?php get_template_part( 'content', 'social-footer' ); ?>
+		<?php get_template_part( 'content', 'social-footer' ); ?>
 
-				<?php comments_template( '', true ); ?>
-<?php else: ?>
-<?php pb_private(); ?>
-<?php endif; ?>
-<?php get_footer(); ?>
-<?php endwhile;?>
+<?php comments_template( '', true ); ?>
+
+<?php
+		get_footer();
+	} // endwhile
+} else {
+	pb_private();
+} // endif
+?>
